22470d11b3b217ea8a964f3ad843160ed27b8036
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-07-03 13:05:49 +0700

    upload modals

--- src/components/elements/UploadModal.tsx
+++ src/components/elements/UploadModal.tsx
@@ -1,5 +1,7 @@
-import { Dialog, Transition } from '@headlessui/react'
-import React, { Fragment } from 'react'
+import { Dialog, Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import React, { Fragment, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface UploadModalProps {
   isModalOpen: any
@@ -10,6 +12,16 @@ const UploadModal: React.FC<UploadModalProps> = ({
   isModalOpen,
   setIsModalOpen,
 }) => {
+  const people = [
+    { id: 1, name: 'Category', unavailable: false },
+    { id: 2, name: 'artwork', unavailable: false },
+    { id: 3, name: 'anime', unavailable: false },
+    { id: 4, name: 'meme', unavailable: true },
+    { id: 5, name: 'photography', unavailable: false },
+    { id: 6, name: 'furry', unavailable: false },
+  ]
+  const [selected, setSelected] = useState(people[0])
+  const { register, handleSubmit } = useForm()
   return (
     <Transition appear show={isModalOpen} as={Fragment}>
       <Dialog
@@ -40,27 +52,84 @@ const UploadModal: React.FC<UploadModalProps> = ({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-red-300 p-6 text-left align-middle shadow-xl transition-all space-y-5">
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  className="text-lg font-medium leading-6 text-gray-900 text-center"
                 >
-                  Payment successful
+                  Upload Image
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
-                </div>
-
-                <div className="mt-4">
+                <input
+                  {...register('date')}
+                  className="w-full h-10 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+                  id="inline-date"
+                  type="file"
+                />
+                <input
+                  {...register('date')}
+                  className="w-full h-10 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+                  id="inline-date"
+                  type="date"
+                />
+                <Listbox value={selected} onChange={setSelected}>
+                  <Listbox.Button className="h-10 relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate">{selected.name}</span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className=" mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {people.map((person, personIdx) => (
+                        <Listbox.Option
+                          key={personIdx}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? 'bg-amber-100 text-amber-900'
+                                : 'text-gray-900'
+                            }`
+                          }
+                          value={person}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <p
+                                className={`block truncate ${
+                                  selected ? 'font-medium' : 'font-normal'
+                                }`}
+                              >
+                                {person.name}
+                              </p>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </Listbox>
+                <div className="flex items-center justify-end">
                   <button
                     type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setIsModalOpen(false)}
+                    className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                   >
-                    Got it, thanks!
+                    Upload
                   </button>
                 </div>
               </Dialog.Panel>
